78efa4a6856e78d2eca3d38577c9578737fa684c
Michelle Minkoff <meminkoff@gmail.com>
2010-03-15 22:45:22 -0500

    updating restructured directories

---
+++ projects/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('projects.galleries.views',
+    (r'galleries/$', 'index'),
+    (r'(?P<id>\d+)/$', 'detail'),
+    (r'search_name', 'search_name'),
+    (r'search_closed', 'search_closed'),
+    (r'search_founded', 'search_founded'),
+    (r'search_media', 'search_media'),
+    (r'search_neighborhood', 'search_neighborhood'),
+    (r'search_outcome', 'search_outcome'),
+    (r'search_zipcode', 'search_zipcode'),
+
+
+)
