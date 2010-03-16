8b0ec695d21b78b0eaa9eb19baed24c61e6121d0
Michelle Minkoff <meminkoff@gmail.com>
2010-03-15 22:42:55 -0500

    rearranging files

--- data/admin.py
+++
@@ -1,4 +0,0 @@
-from galleries.data.models import Galleries1990
-from django.contrib import admin
-
-admin.site.register(Galleries1990)
--- galleries/admin.py
+++
@@ -1,4 +0,0 @@
-from projects.galleries.models import Gallery1990
-from django.contrib import admin
-
-admin.site.register(Gallery1990)
